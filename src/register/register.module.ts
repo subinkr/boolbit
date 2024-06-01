0e88c4994e485dc3b40b2e9b6278f59e1f645de3
subin <github@subin.kr>
2024-06-01 18:30:31 +0900

    FIX: Dependency error

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RegisterService } from './register.service';
 import { RegisterController } from './register.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from 'src/_core/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
+import { AuthModule } from 'src/_common/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserModel]), UsersModule, AuthModule],
   controllers: [RegisterController],
   providers: [RegisterService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from 'src/_core/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserModel])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
