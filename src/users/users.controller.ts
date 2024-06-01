ec7c1d3930b38f7c3d3a8766388bca70fc8c3eb3
subin <github@subin.kr>
2024-06-01 17:45:34 +0900

    ADD: getUser()

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ResGetUser } from './dto/res-get-user.dto';
+import { notFound } from 'src/_core/error/not-found';
 
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get user by ID' })
+  @ApiOkResponse({ type: ResGetUser })
+  @ApiNotFoundResponse(notFound('User not found'))
+  async getUser(@Param('id', ParseIntPipe) id: number): Promise<ResGetUser> {
+    return this.usersService.getUser(id);
+  }
 }
