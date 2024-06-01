08cceeac70754dfb7941a2a6bc085d4e69845244
subin <github@subin.kr>
2024-06-01 18:08:02 +0900

    ADD: localRegister()

---
+++ src/register/dto/req-local-register.dto.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { MockUserModel } from 'src/_mock/entities/user.entity';
+
+export class ReqLocalRegister {
+  @ApiProperty({ example: MockUserModel.defaultUser.username })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  username: string;
+
+  @ApiProperty({ example: 'p@ssw0rd' })
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @ApiProperty({ example: MockUserModel.defaultUser.nickname })
+  @IsNotEmpty()
+  @IsString()
+  nickname: string;
+
+  @ApiProperty({ example: MockUserModel.defaultUser.nickname, required: false })
+  @IsString()
+  image?: string;
+}
--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthService } from 'src/_common/auth/auth.service';
+import { UserModel } from 'src/_core/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { Repository } from 'typeorm';
+import { ReqLocalRegister } from './dto/req-local-register.dto';
 
 @Injectable()
-export class RegisterService {}
+export class RegisterService {
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly userRepository: Repository<UserModel>,
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
+  async localRegister(reqLocalRegister: ReqLocalRegister): Promise<string> {
+    const { username, password } = reqLocalRegister;
+    const existUser = await this.usersService.getUser(username);
+    if (existUser) {
+      throw new ConflictException('User already registered');
+    }
+
+    const hashPassword = await this.authService.hashPassword(password);
+
+    const user = await this.userRepository.save({
+      ...reqLocalRegister,
+      password: hashPassword,
+    });
+    const accessToken = this.authService.signToken(user.id);
+
+    return accessToken;
+  }
+}
