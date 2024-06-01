78916a5f47533fb35b3bcaacfb1a05461551cb58
subin <github@subin.kr>
2024-06-01 18:30:59 +0900

    ADD: Mock accessToken

--- src/_mock/entities/user.entity.ts
+++ src/_mock/entities/user.entity.ts
@@ -12,4 +12,7 @@ export class MockUserModel {
     nickname: 'nickname',
     image: null,
   };
+
+  static accessToken: string =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNzAzNDA5OTA0LCJleHAiOjFlKzUwfQ.BBf7DDbpw-mopP6iPvu8pxc7PoTjCbt5p7h3RPWT_Cw';
 }
--- src/register/dto/res-register.dto.ts
+++ src/register/dto/res-register.dto.ts
@@ -1,3 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { MockUserModel } from 'src/_mock/entities/user.entity';
+
 export class ResRegister {
+  @ApiProperty({ example: MockUserModel.accessToken })
   accessToken: string;
 }
