7d798291313e7840dd56bd77972c3c3c040241e7
subin <github@subin.kr>
2024-06-02 13:32:19 +0900

    MOD: users test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { providers } from 'src/_mock/providers';
+import { MockUserModel } from 'src/_mock/entities/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
+  const { defaultUser } = MockUserModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: providers,
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('Get user', () => {
+    it('USE | getUser', async () => {
+      service.getUser = jest.fn().mockReturnValue(defaultUser);
+      await controller.getUser(defaultUser.id);
+      expect(service.getUser).toHaveBeenCalled();
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { providers } from 'src/_mock/providers';
+import { NotFoundException } from '@nestjs/common';
+import { MockUserModel } from 'src/_mock/entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const { defaultUser } = MockUserModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: providers,
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Get user', () => {
+    it('RUN', async () => {
+      await service.getUser(defaultUser.id);
+    });
+
+    it('RUN', async () => {
+      await service.getUser(defaultUser.username);
+    });
+
+    it('ERR | User not found', async () => {
+      const result = service.getUser(0);
+      await expect(result).rejects.toThrow(NotFoundException);
+    });
   });
 });
