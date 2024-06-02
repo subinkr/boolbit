23b94f0869d5c48241afc39e27fe2ff482816777
subin <github@subin.kr>
2024-06-02 13:16:02 +0900

    ADD: mock data

---
+++ src/_mock/dtos/register/req-local-register.dto.ts
@@ -0,0 +1,8 @@
+import { ReqLocalRegister } from 'src/register/dto/req-local-register.dto';
+
+export const mockReqLocalRegister: ReqLocalRegister = {
+  username: 'new_username',
+  password: 'p@ssw0rd',
+  nickname: 'nickname',
+  image: null,
+};
--- src/_mock/entities/user.entity.ts
+++ src/_mock/entities/user.entity.ts
@@ -8,11 +8,49 @@ export class MockUserModel {
     deletedAt: null,
 
     username: 'username',
-    password: 'password',
+    password: '$2b$10$qNKbTu/9urzFoJ1QrzoaAej7xmAq.a7Bg3tAhKdDE0p.RuVwfppO2',
     nickname: 'nickname',
     image: null,
   };
 
+  static users: UserModel[] = [];
+
   static accessToken: string =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNzAzNDA5OTA0LCJleHAiOjFlKzUwfQ.BBf7DDbpw-mopP6iPvu8pxc7PoTjCbt5p7h3RPWT_Cw';
+
+  findOne({ where: { id, username } }) {
+    const [user] = id
+      ? MockUserModel.users.filter((user) => user.id === id)
+      : MockUserModel.users.filter((user) => user.username === username);
+
+    if (!user) return null;
+
+    return user;
+  }
+
+  findAndCount() {
+    return MockUserModel.users;
+  }
+
+  exist({ where: { id, username } }) {
+    const [user] = id
+      ? MockUserModel.users.filter((user) => user.id === id)
+      : MockUserModel.users.filter((user) => user.username === username);
+
+    if (user) return true;
+
+    return false;
+  }
+
+  save() {
+    MockUserModel.users.push(MockUserModel.defaultUser);
+
+    return MockUserModel.defaultUser;
+  }
+
+  update() {}
+
+  softDelete() {
+    return true;
+  }
 }
