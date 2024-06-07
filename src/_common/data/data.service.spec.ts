9325a96c04aca0d6c1ca099f1967de5f7cf247f1
subin <github@subin.kr>
2024-06-07 18:48:06 +0900

    ADD: Pagination

--- src/_common/data/data.service.spec.ts
+++ src/_common/data/data.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataService } from './data.service';
 import { providers } from 'src/_mock/providers';
+import { emptyFile } from 'src/_mock/dtos/emptyFile';
+import { BoardModel } from 'src/_core/entities/board.entity';
+import { MockBoardModel } from 'src/_mock/entities/board.entity';
 
 describe('DataService', () => {
   let service: DataService;
+  const { boardList } = MockBoardModel;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,7 +17,26 @@ describe('DataService', () => {
     service = module.get<DataService>(DataService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('Upload image', () => {
+    it('RUN | uploadImage', async () => {
+      await service.uploadImage(emptyFile);
+    });
+  });
+
+  describe('Pagination', () => {
+    it('RUN | pagination', () => {
+      const findAndCount: [BoardModel[], number] = [
+        boardList,
+        boardList.length,
+      ];
+      const take = 3;
+      const result = service.pagination(findAndCount, take);
+      const { array, totalPages } = result;
+
+      expect(array).toStrictEqual(findAndCount[0]);
+      expect(totalPages).toEqual(
+        Math.floor((findAndCount[1] && findAndCount[1] - 1) / take) + 1,
+      );
+    });
   });
 });
--- src/_common/data/data.service.ts
+++ src/_common/data/data.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ResPagination } from './dto/res-pagination.dto';
 
 @Injectable()
 export class DataService {
   async uploadImage(file: Express.Multer.File): Promise<string> {
     return `${process.env.HOST}/public/image/${file.filename}`;
+  }
+
+  pagination<T>(findAndCount: [T[], number], take: number): ResPagination<T> {
+    const array = findAndCount[0];
+    const totalPages =
+      Math.floor((findAndCount[1] && findAndCount[1] - 1) / take) + 1;
+
+    return { array, totalPages };
   }
 }
