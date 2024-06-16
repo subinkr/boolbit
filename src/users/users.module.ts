9b946692bc1b0687285c1af12948ebde618ae645
subin <github@subin.kr>
2024-06-16 17:45:34 +0900

    FIX: Auth guard dependency

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,11 +4,13 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from 'src/_core/entities/user.entity';
 import { DataService } from 'src/_common/data/data.service';
+import { AuthService } from 'src/_common/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserModel])],
   controllers: [UsersController],
-  providers: [UsersService, DataService],
+  providers: [UsersService, DataService, AuthService, JwtService],
   exports: [UsersService],
 })
 export class UsersModule {}
